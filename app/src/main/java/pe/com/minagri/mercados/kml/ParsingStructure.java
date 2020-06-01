3b17797ec80b9a674440b3deb2b7bad26a45a7f2
LUIS <luis@LUISs-MacBook-Air.local>
2020-05-31 20:06:42 -0500

    primera version

---
+++ app/src/main/java/pe/com/minagri/mercados/kml/ParsingStructure.java
@@ -0,0 +1,33 @@
+package pe.com.minagri.mercados.kml;
+
+public class ParsingStructure {
+
+    private String name;
+    private String description;
+    private String coordinates;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+
+    }
+}
